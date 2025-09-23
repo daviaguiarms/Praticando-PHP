859e8a8ee79194af15e6004bd058554ece36d431
Davi Aguiar <davi.aguiarms@gmail.com>
2025-09-22 23:49:06 -0300

    Preaticando poo

---
+++ POO/ExercGemini6.php
@@ -0,0 +1,43 @@
+<?php
+
+class Aluno
+{
+    private $nome;
+    private $matricula;
+    private $notas = [];
+
+    public function __construct($nome, $matricula)
+    {
+        $this->nome = $nome;
+        $this->matricula = $matricula;
+    }
+
+    public function addNota($nota)
+    {
+        $this->notas[] = $nota;
+    }
+
+    public function calcularMedia()
+    {
+        return array_sum($this->notas) / count($this->notas);
+    }
+
+    public function status()
+    {
+        if ($this->calcularMedia() >= 7) {
+            return "Aprovado";
+        } else {
+            return 'Reprovado';
+        }
+    }
+}
+
+$objAluno = new Aluno('Davi', 125);
+$objAluno->addNota(6);
+$objAluno->addNota(6);
+$objAluno->addNota(8);
+$objAluno->addNota(4);
+$objAluno->addNota(5);
+$objAluno->calcularMedia();
+echo $objAluno->status() . "\n";
+echo $objAluno->calcularMedia();
